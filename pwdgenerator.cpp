f29cdd3d012d1d0eddd2269ed7a83b9ba3c714d9
Carlo van Driesten <carlo.van.driesten@mytum.de>
2017-05-21 21:42:37 +0200

    work in progress for different keyboards

--- pwdgenerator.cpp
+++ pwdgenerator.cpp
@@ -10,6 +10,55 @@
 #include <conio.h>
 #include <QStringList>
 #include <QtMath>
+#include <vector>
+
+
+std::vector<std::pair<QString, QString>> replacementsGer {
+    { "1", "!" },
+    { "2", "\""},
+    { "3", "§" },
+    { "4", "$" },
+    { "5", "%" },
+    { "6", "&" },
+    { "7", "/" },
+    { "8", "(" },
+    { "9", ")" },
+    { "0", "=" },
+    { "ß", "?" },
+    { "´", "`" },
+    { "+", "*" },
+    { "^", "°" },
+    { "#", "'" },
+    { "-", "_" },
+    { ",", ";" },
+    { ".", ":" }
+};
+
+// german keyboard with united states english settings
+std::vector<std::pair<QString, QString>> replacementsEng {
+    { "1", "!" },
+    { "2", "@"},
+    { "3", "#" },
+    { "4", "$" },
+    { "5", "%" },
+    { "6", "^" },
+    { "7", "&" },
+    { "8", "*" },
+    { "9", "(" },
+    { "0", ")" },
+    { "ß", "_" },
+    { "´", "+" },
+    { "+", "}" },
+    { "^", "°" },
+    //{ "#", "~" },
+    { "-", "?" },
+    { ",", "<" },
+    { ".", ">" },
+    { "Z", "Y" },
+    { "z", "y" },
+    { "@", "Z" },
+    { "#", "z" }
+};
 
 PwdGenerator::PwdGenerator() : QObject()
 {
@@ -371,11 +420,7 @@ QStringList PwdGenerator::generateInitialPwds(const QStringList &initialSet)
                         newSet << newPwd1
                                << newPwd2
                                << newPwd3
-                               << newPwd4
-                               << newPwd1.replace('z','y')
-                               << newPwd2.replace('z','y')
-                               << newPwd3.replace('z','y')
-                               << newPwd4.replace('z','y');
+                               << newPwd4;
 
                         // add right to left
                         QString newPwd5 = (complementPwd.left(pwdLength - k) + basePwd.left(k)).toLower();
@@ -386,11 +431,7 @@ QStringList PwdGenerator::generateInitialPwds(const QStringList &initialSet)
                         newSet << newPwd5
                                << newPwd6
                                << newPwd7
-                               << newPwd8
-                               << newPwd5.replace('z','y')
-                               << newPwd6.replace('z','y')
-                               << newPwd7.replace('z','y')
-                               << newPwd8.replace('z','y');
+                               << newPwd8;
                     }
                 }
             }
@@ -404,24 +445,110 @@ QStringList PwdGenerator::generateResultingPwdList(const QString &pwd)
 {
     QStringList newSet = QStringList();
 
-    QString text = pwd;
+    // GERMAN FIRST
+    QString textGer = pwd;
+    QStringList tmp = QStringList();
 
-    for (int i = 0, n = static_cast<int>(qPow(2, text.length())); i < n; i++) {
-        QString permutation = QString(text.length(), ' ');
-        for (int j =0; j < text.length(); j++) {
-            permutation[j] = (PwdGenerator::isBitSet(i, j)) ? text[j].toUpper() : text[j];
+    for (int i = 0, n = static_cast<int>(qPow(2, textGer.length())); i < n; i++) {
+        QString permutation = QString(textGer.length(), ' ');
+        for (int j = 0; j < textGer.length(); j++) {
+            permutation[j] = (PwdGenerator::isBitSet(i, j)) ? textGer[j].toUpper() : textGer[j];
         }
 
-
+        // Add symbol
         QString newPwd1 = permutation + "!";
         QString newPwd2 = permutation + "?";
         QString newPwd3 = permutation + "!?";
         QString newPwd4 = permutation + "?!";
+        QString newPwd5 = permutation + "!!";
+        QString newPwd6 = permutation + "??";
+        // leave them out for now
+        QString newPwd7 = "!" + permutation;
+        QString newPwd8 = "?" + permutation;
+        QString newPwd9 = "!?" + permutation;
+        QString newPwd10 = "?!" + permutation;
+        QString newPwd11 = "!!" + permutation;
+        QString newPwd12 = "??" + permutation;
 
-        newSet << newPwd1 << newPwd2 << newPwd3 << newPwd4;
+        tmp    << newPwd1
+               << newPwd2
+               << newPwd3
+               << newPwd4
+               << newPwd5
+               << newPwd6
+               << newPwd7
+               << newPwd8
+               << newPwd9
+               << newPwd10
+               << newPwd11
+               << newPwd12;
+
+        // account for number capslock german keyboard layout
+        // normal characters are included above
+        // numbers and symbols have to be delt with
+        QString modifiedPermutation = permutation;
+
+        for ( auto const &r : replacementsGer) {
+            modifiedPermutation.replace(r.first, r.second);
+        }
+
+        // Add symbol
+        newPwd1 = modifiedPermutation + "1";
+        newPwd2 = modifiedPermutation + "ß";
+        newPwd3 = modifiedPermutation + "1ß";
+        newPwd4 = modifiedPermutation + "ß1";
+        newPwd5 = modifiedPermutation + "11";
+        newPwd6 = modifiedPermutation + "ßß";
+        // leave them out for now
+        newPwd7 = "1" + modifiedPermutation;
+        newPwd8 = "ß" + modifiedPermutation;
+        newPwd9 = "1ß" + modifiedPermutation;
+        newPwd10 = "ß1" + modifiedPermutation;
+        newPwd11 = "11" + modifiedPermutation;
+        newPwd12 = "ßß" + modifiedPermutation;
+
+        tmp    << newPwd1
+               << newPwd2
+               << newPwd3
+               << newPwd4
+               << newPwd5
+               << newPwd6
+               << newPwd7
+               << newPwd8
+               << newPwd9
+               << newPwd10
+               << newPwd11
+               << newPwd12;
+
     }
 
+    // store german passwords
+    newSet << tmp;
 
+    // NOW ENGLISH
+    for (int j = 0; j < tmp.length(); j++) {
+
+        // HACK
+        if(!tmp[j].contains("#")) {
+            tmp[j].replace("y", "#");
+        }
+        else {
+            qDebug() << "HACK Failed";
+        }
+        if(!tmp[j].contains("@")) {
+            tmp[j].replace("Y", "@");
+        }
+        else {
+            qDebug() << "HACK Failed";
+        }
+
+        for ( auto const &r : replacementsEng) {
+            tmp[j].replace(r.first, r.second);
+        }
+        newSet << tmp[j];
+    }
+
+    newSet.removeDuplicates();
     return newSet;
 }
 
